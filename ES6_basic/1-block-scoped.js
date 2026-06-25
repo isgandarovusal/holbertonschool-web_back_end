29f57159af121fba2d83e5b13907ccdb962b4e6c
isgandarovusal <isgandarovusal@gmail.com>
2026-06-25 06:24:26 +0000

    Fix Task 1: Strict let block scoping with original variable names

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,1 +1,1 @@
-export default function taskBlock(trueOrFalse) { let task = false; let task2 = true; if (trueOrFalse) { const task3 = true; const task4 = false; } return [task, task2]; }
+export default function taskBlock(trueOrFalse) { let task = false; let task2 = true; if (trueOrFalse) { let task = true; let task2 = false; } return [task, task2]; }
