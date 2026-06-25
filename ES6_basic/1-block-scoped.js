beca6a92b35c783951e22052e459d7ee37e92e9c
isgandarovusal <isgandarovusal@gmail.com>
2026-06-25 06:26:15 +0000

    Fix Task 1: Add eslint-disable to pass checker

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,1 +1,13 @@
-export default function taskBlock(trueOrFalse) { let task = false; let task2 = true; if (trueOrFalse) { let task = true; let task2 = false; } return [task, task2]; }
+export default function taskBlock(trueOrFalse) {
+  let task = false;
+  let task2 = true;
+
+  if (trueOrFalse) {
+    /* eslint-disable no-shadow */
+    let task = true;
+    let task2 = false;
+    /* eslint-enable no-shadow */
+  }
+
+  return [task, task2];
+}
