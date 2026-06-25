8b77e1260d0939d6864a11b55745c99d338428c8
isgandarovusal <isgandarovusal@gmail.com>
2026-06-25 06:27:03 +0000

    Fix Task 1: Use let for outer and const for inner block variables

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,13 +1,1 @@
-export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
-
-  if (trueOrFalse) {
-    /* eslint-disable no-shadow */
-    let task = true;
-    let task2 = false;
-    /* eslint-enable no-shadow */
-  }
-
-  return [task, task2];
-}
+export default function taskBlock(trueOrFalse) { let task = false; let task2 = true; if (trueOrFalse) { const task = true; const task2 = false; } return [task, task2]; }
