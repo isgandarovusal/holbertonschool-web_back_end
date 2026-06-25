e3bd04709f1c0808bb93c8bff1627a9100e06237
isgandarovusal <isgandarovusal@gmail.com>
2026-06-25 07:39:26 +0000

    Fix: Move all task files into the correct ES6_basic directory

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,3 +1,3 @@
-export default function getSumOfHoods(initialNumber, expansion1 = 89, expansion2 = 19) {
-  return initialNumber + expansion1 + expansion2;
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+  return initialNumber + expansion1989 + expansion2019;
 }
