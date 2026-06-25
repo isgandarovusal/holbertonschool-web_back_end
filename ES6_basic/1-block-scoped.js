8fb1f6c9209896eca3845ca101ce985021b1df07
isgandarovusal <isgandarovusal@gmail.com>
2026-06-25 07:32:34 +0000

    Verified Natavan code sync for tasks 1, 3, 6, 8, 9

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
   let task = false;
-  let taak2 = true;
+  let task2 = true;
 
   if (trueOrFalse) {
     const task = true;
-    const taak2 = false;
+    const task2 = false;
   }
 
-  return [task, taak2];
+  return [task, task2];
 }
