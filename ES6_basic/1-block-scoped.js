acacbfa7e4dc56b91702453c813124126695b1d1
isgandarovusal <isgandarovusal@gmail.com>
2026-06-25 06:18:06 +0000

    Fix Task 1: Clean and direct block scoping

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,10 +1,1 @@
-export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var taak2 = "is any";
-
-  if (trueOrFalse) {
-    var task = true;
-    var taak2 = "is none";
-  }
-
-  return [task, task2];J}
+export default function taskBlock(trueOrFalse) { const task = false; const task2 = 'is any'; if (trueOrFalse) { const task = true; const task2 = 'is none'; } return [task, task2]; }
