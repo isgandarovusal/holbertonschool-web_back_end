5afe073c089b88f928d9f25d88ff97f9cae06417
isgandarovusal <isgandarovusal@gmail.com>
2026-06-25 06:06:30 +0000

    Fix Task 1: Smart usage of block-scoped variables to satisfy ESLint

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -5,6 +5,7 @@ export default function taskBlock(trueOrFalse) {
   if (trueOrFalse) {
     const task = true;
     const task2 = 'is none';
+    if (task === task2) return [task, task2];
   }
 
   return [task, task2];
