f99ec51b50562a2daa3134e3f3b58bccd52b0592
isgandarovusal <isgandarovusal@gmail.com>
2026-06-25 07:11:55 +0000

    Ultimate fix for tasks 1, 3, 6, 8, 9 with strict formatting

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,1 +1,3 @@
-export default function getSumOfHoods(initialNumber, expansion1 = 89, expansion2 = 19) { return initialNumber + expansion1 + expansion2; }
+export default function getSumOfHoods(initialNumber, expansion1 = 89, expansion2 = 19) {
+  return initialNumber + expansion1 + expansion2;
+}
--- ES6_basic/8-getBudgetForCurrentYear.js
+++ ES6_basic/8-getBudgetForCurrentYear.js
@@ -1,2 +1,13 @@
 import getBudgetObject from './7-getBudgetObject';
-export default function getBudgetForCurrentYear(income, gdp, capita) { const date = new Date(); const year = date.getFullYear(); const budget = getBudgetObject(income, gdp, capita); return { [`income-${year}`]: budget.income, [`gdp-${year}`]: budget.gdp, [`capita-${year}`]: budget.capita }; }
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const date = new Date();
+  const year = date.getFullYear();
+  const budget = getBudgetObject(income, gdp, capita);
+
+  return {
+    [`income-${year}`]: budget.income,
+    [`gdp-${year}`]: budget.gdp,
+    [`capita-${year}`]: budget.capita,
+  };
+}
